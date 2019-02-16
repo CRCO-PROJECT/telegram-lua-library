62e4b0a2b01fd09a14c2c32d0f3e1a01f69c4b2a
Amir Bagheri <32092233+KhodeAmir@users.noreply.github.com>
2019-02-16 19:01:17 +0330

    Create sendMediaGroup.lua

---
+++ Example/sendMediaGroup.lua
@@ -0,0 +1,20 @@
+    token = '' -- TOKEN
+      MsgTime = os.time() - 5
+            local BOT, MOD = require("./LIB/LIB-LUA").initialize(token)
+                    MOD.getMessage_ = function (msg)
+                    Array =   {  -- Array of InputMediaPhoto
+                        {                                                                                                                   
+                          caption = "`Name` : *Amir*",  -- Caption                                                                              
+                          media = "AgADBAADxKcxG-DkFCl5Tkr2_ZKWkIJFoBoABErtTEpRwi7Ms1sHAAEC",   -- File id or url                                       
+                          parse_mode = "Markdown",        --MarkDown                                                                                   
+                          type = "photo"      --Type input                                                                                           
+                        }                                                                                                                   
+                      }    
+
+    -- InputMediaVideo  > https://core.telegram.org/bots/api#inputmediavideo
+
+    BOT.sendMedia(msg.on.chat_id,Array,msg.on.id)
+end
+end
+    
+            MOD.RUNING()
