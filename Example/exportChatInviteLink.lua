ead134d60b3427249ef925a64e692aa04f9b80b5
Amir Bagheri <32092233+KhodeAmir@users.noreply.github.com>
2019-02-16 23:43:39 +0330

    Create exportChatInviteLink.lua

---
+++ Example/exportChatInviteLink.lua
@@ -0,0 +1,13 @@
+  token = ' ' -- TOKEN
+     BOT, MOD = require("./LIB/telegram-bot").initialize(token)
+        MOD.getMessage_ = function (msg)
+          if msg.content.text == 'newlink' then
+            if BOT.exportChatInviteLink(msg.on.chat_id) then
+                link = BOT.exportChatInviteLink(msg.on.chat_id).result  
+                    text = 'New Link created\n '..link
+                      else
+              text = 'Bot Can't create or revoke link'
+        end
+  BOT.sendText(msg.on.chat_id,msg.on.id,text,'HTML')
+      end
+  MOD.RUNING()
