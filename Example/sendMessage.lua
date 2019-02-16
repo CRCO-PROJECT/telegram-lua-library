9eeb0c559287402ce814d04d8e92350d81bdfba7
Amir Bagheri <32092233+KhodeAmir@users.noreply.github.com>
2019-02-16 18:38:25 +0330

    Create sendMessage.lua

---
+++ Example/sendMessage.lua
@@ -0,0 +1,9 @@
+    token = '' -- TOKEN
+                local BOT, MOD = require("./LIB/LIB-LUA").initialize(token)
+                        MOD.getMessage_ = function (msg)
+if msg.content.text == "ping"  then
+        BOT.sendText(msg.on.chat_id,msg.on.reply_to_message_id,'`PONG`','md')
+            end
+end
+
+        MOD.RUNING()
