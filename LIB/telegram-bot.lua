b66d7fdce0e3725102e1181ebaaef3afcfe64eec
Amir Bagheri <32092233+KhodeAmir@users.noreply.github.com>
2019-02-16 22:31:38 +0330

    Create telegram-bot.lua

---
+++ LIB/telegram-bot.lua
@@ -0,0 +1,22 @@
+
+ require("./LIB/LIB-LUA")   
+local RUNING = function(update_id,limit, timeout)
+    while true do 
+        timeout = timeout or 0
+            limit = limit or 1 
+                update_id = update_id or 0
+                    updates = MAIN.getUpdate(update_id,limit,timeout)
+                        if updates and updates.result then
+                            for i=1, #updates.result do
+                                    update = updates.result[i]
+                                        update_id = update.update_id + 1
+
+                                            checkUpdate(update)
+                                    end
+                        end
+                end
+        end
+
+    MOD.RUNING = RUNING
+        return CRCO
+ 
