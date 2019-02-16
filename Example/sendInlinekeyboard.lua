247bcc04bc06f2d2f311ceed9fdf654e210a742e
Amir Bagheri <32092233+KhodeAmir@users.noreply.github.com>
2019-02-17 00:02:58 +0330

    Update sendInlinekeyboard.lua

--- Example/sendInlinekeyboard.lua
+++ Example/sendInlinekeyboard.lua
@@ -5,7 +5,7 @@
                key = {}
                   key.inline_keyboard = {
         {
-    {text = 'PING', callback_data="pong_2"}
+    {text = 'PING', callback_data="pong_1"}
     }
 }
       BOT.sendInline(msg.on.chat_id,msg.on.sender_firstname,key, msg.on.id, 'md')
