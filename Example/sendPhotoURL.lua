73f33dc2ea59eea5e732f862b6c4a60312953238
Amir Bagheri <32092233+KhodeAmir@users.noreply.github.com>
2019-03-20 22:17:20 +0330

    Create sendPhotoURL.lua

---
+++ Example/sendPhotoURL.lua
@@ -0,0 +1,9 @@
+  token = '' -- TOKEN
+       local BOT, MOD = require("./LIB/telegram-bot").initialize(token)
+            MOD.getMessage_ = function (msg)
+    if msg.content.text == 'photo' then
+    BOT.sendPhotoURL(msg.on.chat_id,msg.on.id,'http://s8.picofile.com/file/8312881426/photo_2017_09_19_11_51_39.jpg','*test*','md')
+
+end
+         end
+                MOD.RUNING()
