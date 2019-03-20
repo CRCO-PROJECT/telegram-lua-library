52ce6ef37af06124237666bbb923c7746c99077d
Amir Bagheri <32092233+KhodeAmir@users.noreply.github.com>
2019-03-20 22:06:56 +0330

    Create sendPhoto.lua

---
+++ Example/sendPhoto.lua
@@ -0,0 +1,9 @@
+
+  token = '' -- TOKEN
+     BOT, MOD = require("./LIB/telegram-bot").initialize(token)
+        MOD.getMessage_ = function (msg)
+          if msg.content.text == 'photo' then
+            BOT.sendPhoto(msg.on.chat_id,msg.on.id,'./test.jpg','*test*','md')
+    end
+end
+      MOD.RUNING()
