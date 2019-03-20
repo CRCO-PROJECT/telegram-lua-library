6d97ad2df9600a973e202b6279405477a7e67c85
Amir Bagheri <32092233+KhodeAmir@users.noreply.github.com>
2019-03-20 20:43:23 +0330

    Create getChat.lua

---
+++ Example/getChat.lua
@@ -0,0 +1,43 @@
+         token = '' -- TOKEN
+             local BOT, MOD = require("./LIB/telegram-bot").initialize(token)
+                MOD.getMessage_ = function (msg)
+     if msg.content.text == 'ping' then
+          BOT.getChat(msg.on.chat_id)
+         
+    end
+         end
+    MOD.RUNING()
+--[[ result
+ 
+{
+  ok = true,
+  result = {
+    description = "CRCO",
+    id = -1001418960037,
+    photo = {
+      big_file_id = "AQADBAATxYYpGwAESjbcmqW5Q6FmcwMAAQI",
+      small_file_id = "AQADBAATxYYpGwAEPnb4YwHoUBlkcwMAAQI"
+    },
+    pinned_message = {
+      chat = {
+        id = -1001418960037,
+        title = "CRCO",
+        type = "supergroup"
+      },
+      date = 1553101848,
+      from = {
+        first_name = "Parzival",
+        id = 603841307,
+        is_bot = false,
+        language_code = "fa",
+        username = "ZendegiSefareshi"
+      },
+      message_id = 33,
+      text = "CRCO PROJECT"
+    },
+    title = "CRCO",
+    type = "supergroup"
+  }
+}
+
+]]--
