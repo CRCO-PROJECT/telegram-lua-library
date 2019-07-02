642ad958699fb1a0ac96ec50bc6f68fd5482713f
Amir Bagheri <32092233+KhodeAmir@users.noreply.github.com>
2019-07-02 11:52:42 +0430

    Update sendMessage.lua

--- Example/sendMessage.lua
+++ Example/sendMessage.lua
@@ -1,5 +1,5 @@
     token = '' -- TOKEN
-                local BOT, MOD = require("./LIB/telegram-bot").initialize(token)
+                local BOT, MOD = require("Telegram-Bot.LIB.telegram-bot").initialize(token)
                         MOD.getMessage_ = function (msg)
 if msg.content.text == "ping"  then
         BOT.sendText(msg.on.chat_id,msg.on.reply_to_message_id,'`PONG`','md')
