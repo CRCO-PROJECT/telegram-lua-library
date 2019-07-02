852072b9f1e5742e608cab9a4b396051913b3845
Amir Bagheri <32092233+KhodeAmir@users.noreply.github.com>
2019-07-02 11:54:47 +0430

    Update promoteChatMember.lua

--- Example/promoteChatMember.lua
+++ Example/promoteChatMember.lua
@@ -1,5 +1,5 @@
   token = '' -- TOKEN
-       local BOT, MOD = require("./LIB/telegram-bot").initialize(token)
+       local BOT, MOD = require("Telegram-Bot.LIB.telegram-bot").initialize(token)
             MOD.getMessage_ = function (msg)
     if msg.content.text == 'promoteuser' then
     can_change_info = 0
