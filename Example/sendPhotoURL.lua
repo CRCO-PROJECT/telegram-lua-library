e7ecef377a661e304a331c5282cf1d358ed8edc6
Amir Bagheri <32092233+KhodeAmir@users.noreply.github.com>
2019-07-03 01:53:59 +0430

    Update sendPhotoURL.lua

--- Example/sendPhotoURL.lua
+++ Example/sendPhotoURL.lua
@@ -2,7 +2,13 @@
        local BOT, MOD = require("Telegram-Bot.LIB.telegram-bot").initialize(token)
             MOD.getMessage_ = function (msg)
     if msg.content.text == 'photo' then
-    BOT.sendPhotoURL(msg.on.chat_id,msg.on.id,'http://s8.picofile.com/file/8312881426/photo_2017_09_19_11_51_39.jpg','*test*','md')
+    key = {}
+                               key.inline_keyboard = {
+                     {
+                 {text = 'OK', callback_data="__"}
+                 }
+             }
+BOT.sendPhotoURL(msg.on.chat_id,msg.on.id,'http://s8.picofile.com/file/8312881426/photo_2017_09_19_11_51_39.jpg','*test*','true',key,'md')
 
 end
          end
