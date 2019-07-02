3f394eab449a952cde5b095e672785fb34314b57
Amir Bagheri <32092233+KhodeAmir@users.noreply.github.com>
2019-07-02 11:53:44 +0430

    Update sendDocumentURL.lua

--- Example/sendDocumentURL.lua
+++ Example/sendDocumentURL.lua
@@ -1,5 +1,5 @@
   token = '' -- TOKEN
-       local BOT, MOD = require("./LIB/telegram-bot").initialize(token)
+       local BOT, MOD = require("Telegram-Bot.LIB.telegram-bot").initialize(token)
             MOD.getMessage_ = function (msg)
     if msg.content.text == 'photo_file' then
     BOT.sendDocumentURL(msg.on.chat_id,msg.on.id,'http://s8.picofile.com/file/8312881426/photo_2017_09_19_11_51_39.jpg','*test*','md')
