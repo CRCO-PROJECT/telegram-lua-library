56477355320c76a2cd9b02b7472ce580f2ed6a43
Amir Bagheri <32092233+KhodeAmir@users.noreply.github.com>
2019-07-02 11:54:05 +0430

    Update sendDocumentPath.lua

--- Example/sendDocumentPath.lua
+++ Example/sendDocumentPath.lua
@@ -1,5 +1,5 @@
   token = '' -- TOKEN
-     BOT, MOD = require("./LIB/telegram-bot").initialize(token)
+     BOT, MOD = require("Telegram-Bot.LIB.telegram-bot").initialize(token)
         MOD.getMessage_ = function (msg)
           if msg.content.text == 'file' then
             BOT.sendDocumentPath(msg.on.chat_id,msg.on.id,'test.txt','*test*','md')
