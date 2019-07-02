b1bf5e579dcfb4f2dabae585e101538b09a8b470
Amir Bagheri <32092233+KhodeAmir@users.noreply.github.com>
2019-07-02 11:50:38 +0430

    Update answerInlineQuery.lua

--- Example/answerInlineQuery.lua
+++ Example/answerInlineQuery.lua
@@ -1,5 +1,5 @@
   token = '' -- TOKEN
-   BOT, MOD = require("./LIB/telegram-bot").initialize(token)
+   BOT, MOD = require("Telegram-Bot.LIB.telegram-bot").initialize(token)
     MOD.get_inline_query = function (msg)
      if msg.content.text == 'pong' then
       keyboard = {}
