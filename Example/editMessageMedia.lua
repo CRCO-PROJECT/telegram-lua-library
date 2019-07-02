03d798097e2679cf2a505d3d78c2870d893d8126
Amir Bagheri <32092233+KhodeAmir@users.noreply.github.com>
2019-07-03 01:51:07 +0430

    Create editMessageMedia.lua

---
+++ Example/editMessageMedia.lua
@@ -0,0 +1,20 @@
+
+  token = '' -- TOKEN
+     BOT, MOD = require("Telegram-Bot.LIB.telegram-bot").initialize(token)
+        MOD.getMessage_ = function (msg)
+          if msg.content.text == 'photo' then
+            BOT.sendPhoto(msg.on.chat_id,msg.on.id,'./test.jpg','*test*','md')
+           
+    end
+     if msg.content.text == 'edit photo' then
+        Array =   {  -- Array of InputMediaPhoto
+                     type = "photo",
+                     media = 'http://s8.picofile.com/file/8312881426/photo_2017_09_19_11_51_39.jpg',
+                     caption = 'test',
+                     parse_mode = "Markdown"                                                                                        --Type input                                                                                           
+       }     
+     BOT.editMessageMedia(msg.on.chat_id,msg.on.id,Array,key)
+           
+    end
+end
+      MOD.RUNING()
